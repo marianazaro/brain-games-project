9779c6eaeee3630f9984ac8868d3f3be55eeb55f
maria <marianazarova892@gmail.com>
2022-11-15 14:12:42 +0300

    add new games calculator

--- games/calc.js
+++ games/calc.js
@@ -1,46 +1,45 @@
 import readlineSync from 'readline-sync';
+import {arrOfRandomNumbers} from '../src/functionForGames.js';
 console.log('Welcome to the Brain Games!');
     const userName = readlineSync.question('May I have your name? ');
     console.log(`Hi, ${userName}!`)
 
 console.log('What is the result of the expression?');
 
-const arrOfRandomNumbers = () => {
-    const numbers = [];
-    while (numbers.length < 10) {
-        const randomNumbers = Math.ceil(Math.random() * 100)
-        let found = false;
-        for (let i = 0; i < numbers.length; i += 1) {
-            if (numbers[i] === randomNumbers) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) {numbers[numbers.length]=randomNumbers};
+
+
+const transformOperation = (operator, num1, num2) => {
+    let result = 0;
+    if (operator === '+') {
+        result = num1 + num2;
+    } else if (operator === '-') {
+        result = num1 - num2;
+    } else if (operator === '*') {
+        result = num1 * num2;
     }
-    return numbers;
+    return String(result);
 };
 
 const playGameCalc = (iter = 0) => {
-    const index = () => Math.round(Math.random() * 10);
     const mathOperations = ['+', '-', '*'];
     const numbers = arrOfRandomNumbers();
-    const num1 = numbers[index()];
-    const num2 = numbers[index()];
-    const indexOfOperations = () => index() <= 2 ? index : indexOfOperations;
-    const operation = mathOperations[indexOfOperations]; 
-    console.log(`Question: ${num1} ${operation} ${num2}`);
-    const answer = num1 + operation + num2;
-
+    const index1 = Math.round(Math.random() * 9);
+    const index2 = Math.round(Math.random() * 9);
+    const num1 = numbers[index1];
+    const num2 = numbers[index2];
+    const indexOfOperator = Math.floor(Math.random() * 3);
+    const operator = mathOperations[indexOfOperator]; 
+    const answer = transformOperation(operator, num1, num2);
+    console.log(`Question: ${num1} ${operator} ${num2}`);
+    
     const userAnswer = readlineSync.question('Your answer: ');
-
     if (iter === 2) {
         return console.log(`Congratulations, ${userName}!`);
     };
     
     if (answer === userAnswer) {
         console.log('Correct!')
-        playGameEven(iter += 1);
+        playGameCalc(iter += 1);
     } else if (answer != userAnswer) {
         return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${userName}`);
     }
