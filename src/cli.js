a0f0a5dccc224fba461ed28fcd0f6a76a1cf4799
maria <marianazarova892@gmail.com>
2022-11-16 18:31:46 +0300

    change hi on hello

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 const greeting = () => {
     const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hi, ${userName}!`)
+    console.log(`Hello, ${userName}!`)
 };
 export {greeting};
